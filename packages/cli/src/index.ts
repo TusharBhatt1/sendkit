5070416c6d0c6d9dc857994264f5be7a10d189cb
Tushar <tusharbhatt0135@gmail.com>
2026-06-28 19:59:31 +0530

    publish cli package

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from "commander";
 import { sendTelegramMessage } from "@tb-eng/sendkit-core";
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from "node:fs";
